3f88a49532640975abfb1f425d9b0df4c99bb78a
Jhoan Buitrago <jhoanuitrago@gmail.com>
2026-07-21 18:57:41 -0500

    feat: Fetch wayland sessions

--- scripts/system_info.gd
+++ scripts/system_info.gd
@@ -1,6 +1,7 @@
 class_name SystemInfo
 extends RefCounted
 
+# TODO: Implement X11 sessions?
 # TODO: I don't think this a standard on every distro. Probably need to check other places
 const WAYLAND_SESSIONS_PATH := "/usr/share/wayland-sessions/"
 # NOTE: not sure if these values are always correct
@@ -11,7 +12,7 @@ const MAX_USER_ID := 60000
 static func get_users() -> Array[String]:
 	# HACK: Use fake data for debugging
 	if not OS.has_feature("linux") or OS.has_feature("editor"):
-		return ["mocky", "stubby", "faky"]
+		return ["fakeuser", "stubby", "godoty"]
 
 	var output: Array[String] = []
 	var exit_code := OS.execute("getent", PackedStringArray(["passwd"]), output)
@@ -32,3 +33,74 @@ static func get_users() -> Array[String]:
 			users.append(name)
 
 	return users
+
+
+# NOTE: very simple .desktop file parsing. Maybe it will be necessary to improve
+# this later.
+## Searches for wayland sessions in "/usr/share/wayland-sessions/" and
+## performs a very simple parsing of .desktop files.[br][br]
+## Returns an array of dictionaries where each dictionary has the desktop file keys.
+static func get_wayland_sessions() -> Array[Dictionary]:
+	# HACK: Use fake data for debugging
+	if not OS.has_feature("linux") or OS.has_feature("editor"):
+		return _stub_wayland_sessions()
+
+	var sessions: Array[Dictionary] = []
+
+	var dir := DirAccess.open(WAYLAND_SESSIONS_PATH)
+	if not dir:
+		var error := DirAccess.get_open_error()
+		push_error("Failed to open \"%s\": %s" % [WAYLAND_SESSIONS_PATH, error_string(error)])
+		return sessions
+
+	for file_name in dir.get_files():
+		var file_path := WAYLAND_SESSIONS_PATH + file_name
+		var file := FileAccess.open(file_path, FileAccess.READ)
+
+		if not file:
+			var error := FileAccess.get_open_error()
+			push_error("Failed to open \"%s\": %s" % [file_path, error_string(error)])
+			continue
+
+		var session := _parse_desktop_file(file)
+		sessions.append(session)
+
+	return sessions
+
+
+static func _parse_desktop_file(file: FileAccess) -> Dictionary:
+	var session := { }
+	var length := file.get_length()
+	while file.get_position() < length:
+		var line := file.get_line()
+
+		# Skip comments
+		if line.begins_with("#"):
+			continue
+		# Skip section declaration
+		if line.begins_with("["):
+			continue
+		if line.is_empty():
+			continue
+
+		var key_value := line.split("=", true, 1)
+		session[key_value[0]] = key_value[1]
+
+	return session
+
+
+static func _stub_wayland_sessions() -> Array[Dictionary]:
+	return [
+		{
+			"Name": "MockPlasma (Wayland)",
+			"Exec": "/usr/lib/plasma-dbus-run-session-if-needed /usr/bin/startplasma-wayland",
+		},
+		{
+			"Name": "MockHyprland",
+			"Exec": "/usr/bin/start-hyprland",
+		},
+		{
+			"Name": "MockSway",
+			"Exec": "sway",
+		},
+	]
