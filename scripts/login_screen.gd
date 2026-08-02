27ec368331248b4ee980fffe53065285d9c68d68
Jhoan Buitrago <jhoanuitrago@gmail.com>
2026-08-02 15:28:26 -0500

    refactor: create greeter class for state validation and signals

--- scripts/login_screen.gd
+++ scripts/login_screen.gd
@@ -1,26 +1,26 @@
 extends Node
 
-# TODO: implement state validation
-var client := GreetdClient.new()
-var is_authenticating := false
-
 
 func _ready() -> void:
 	# TODO: might remove this and make it start fullscreen from the start
 	if OS.has_feature("template"):
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
 
-	add_child(client)
-
 	_init_sessions()
 	_init_users()
 
 	%AuthAnswer.text_submitted.connect(_on_auth_answer_submitted)
-	%LogIn.pressed.connect(login)
+	%LogIn.pressed.connect(_on_login_pressed)
 	%SessionSelect.item_selected.connect(_on_session_selected)
 	%Username.item_selected.connect(_on_user_selected)
 	%Shutdown.pressed.connect(_on_shutdown_pressed)
 	%Restart.pressed.connect(_on_restart_pressed)
+
+	Greeter.auth_prompt_received.connect(_on_auth_prompt_received)
+	Greeter.auth_completed.connect(_on_auth_completed)
+	Greeter.login_failed.connect(_on_login_failed)
+	Greeter.session_started.connect(_on_session_started)
+	Greeter.error.connect(_on_error)
 
 	%AuthAnswer.grab_focus() # Automatically focus the password field for quick login.
 	get_tree().set_auto_accept_quit(false) # Handle quit manually
@@ -32,50 +32,53 @@ func _notification(what: int) -> void:
 		_quit()
 
 
-func login() -> void:
-	# Prevent multiple simultaneous login attempts
-	if is_authenticating:
-		return
-
-	is_authenticating = true
+func _on_login_pressed() -> void:
 	_set_ui_enabled(false)
 	%Alert.text = "Authenticating..."
 
 	var username: String = %Username.text
-	var password: String = %AuthAnswer.text
-	var response := await client.create_session(username)
+	Greeter.login(username)
 
-	if response is GreetdError:
-		%Alert.text = response.error_description
-		_log_info(response.error_description)
-		is_authenticating = false
-		_set_ui_enabled(true)
-		return
-	elif response is GreetdAuthMessage:
-		_log_info("%s - %s" % [response.auth_message_type_string(), response.auth_message])
+
+func _on_auth_prompt_received(message: String, type: GreetdAuthMessage.Type) -> void:
+	_log_info("%s - %s" % [GreetdAuthMessage.Type.keys()[type], message])
+
+	if type == GreetdAuthMessage.Type.SECRET or type == GreetdAuthMessage.Type.VISIBLE:
+		var input: String = %AuthAnswer.text
+		Greeter.submit_input(input)
 	else:
-		# TODO if the response is a success then we can start the session
-		# How does that happen? Users without passwords?
-		pass
-
-	_log_info("Session created")
-
-	response = await client.answer_auth_message(password)
-
-	_on_auth_complete(response)
+		# Acknowledge PAM Info/Error messages automatically (send empty response)
+		_log_info("PAM message: %s" % message)
+		Greeter.submit_input("")
 
 
-func cancel_session() -> void:
-	var response := await client.cancel_session()
-	if response is GreetdError:
-		_log_info(response.error_description)
-		return
+func _on_auth_completed() -> void:
+	_log_info("Authentication successful")
 
-	%AuthAnswer.call_deferred("grab_focus")
-	_log_info("Canceled successfully")
+	var cmd: String = %SessionSelect.get_selected_metadata()
+	Greeter.start_session([cmd])
 
 
-## Get the list of users and select the last user that logged in.
+func _on_login_failed(reason: String) -> void:
+	%Alert.text = reason
+	_log_info(reason)
+	_set_ui_enabled(true)
+	%AuthAnswer.grab_focus()
+
+
+func _on_session_started() -> void:
+	_log_info("Session started")
+	_quit()
+
+
+func _on_error(description: String) -> void:
+	%Alert.text = description
+	_log_info(description)
+	_set_ui_enabled(true)
+	%AuthAnswer.grab_focus()
+
+
+# Get the list of users and select the last user that logged in.
 func _init_users():
 	var last_user: String = Config.last_user
 	var users := SystemInfo.get_users()
@@ -87,7 +90,7 @@ func _init_users():
 			%Username.select(i)
 
 
-## Get a list of sessions and select the last used session.
+# Get a list of sessions and select the last used session.
 func _init_sessions():
 	var last_session = Config.last_session
 
@@ -116,42 +119,7 @@ func _on_user_selected(_index: int):
 
 ## When pressing "Enter" run the login
 func _on_auth_answer_submitted(_password):
-	login()
-
-
-func _on_auth_complete(response: GreetdResponse) -> void:
-	if response is GreetdError:
-		%Alert.text = response.error_description
-		_log_info(response.error_description)
-		cancel_session()
-		is_authenticating = false
-		_set_ui_enabled(true)
-		return
-	elif response is GreetdAuthMessage:
-		# TODO: Instead of canceling the session, we should continue asking for stuff if necessary
-		_log_info(
-			"GreetdAuthMessage: %s - %s" % [response.auth_message_type, response.auth_message]
-		)
-		cancel_session()
-		is_authenticating = false
-		_set_ui_enabled(true)
-		return
-	else:
-		_log_info("Auth answered")
-
-	# TODO: Only do this if the answer is a success
-	var cmd = %SessionSelect.get_selected_metadata()
-	response = await client.start_session([cmd])
-	if response is GreetdError or response is GreetdClientError:
-		%Alert.text = response.error_description
-		_log_info(response.error_description)
-		cancel_session()
-		is_authenticating = false
-		_set_ui_enabled(true)
-		return
-
-	_log_info("Session started")
-	_quit()
+	_on_login_pressed()
 
 
 func _set_ui_enabled(enabled: bool) -> void:
